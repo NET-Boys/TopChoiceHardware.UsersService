01c9d96f2db30ff4110b571526cad3efc1b035b9
Matías Salinas <salinasmatias1990@gmail.com>
2021-09-22 17:48:06 -0300

    Pushing changes to repo

---
+++ TopChoiceHardware.UsersService/Controllers/UserController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TopChoiceHardware.UsersService.Application.Services;
+using TopChoiceHardware.UsersService.Domain.DTOs;
+using TopChoiceHardware.UsersService.Domain.Entities;
+
+namespace TopChoiceHardware.UsersService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUsuarioService _service;
+
+        public UserController(IUsuarioService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Usuario), StatusCodes.Status201Created)]
+        public IActionResult Post(UsuarioDto usuario)
+        {
+            try
+            {
+                return new JsonResult(_service.CreateUsuario(usuario)) { StatusCode = 201 };
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            try
+            {
+                var usuarios = _service.GetUsuarios();
+
+                return Ok(usuarios);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(int id)
+        {
+            try
+            {
+                var usuario = _service.GetUsuarioById(id);
+                if(usuario == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(usuario);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
